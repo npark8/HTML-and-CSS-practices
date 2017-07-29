d833e06d73d99bafefbb3c7d49900bcd9f7d5641
npark8 <napark8@gmail.com>
2017-07-29 01:47:19 -0400

    uploading HTML and CSS files

---
+++ module4 - Implementing Program Flow using JavaScript/exercises/exercise7 - Creating a Web Worker/js/script.js
@@ -0,0 +1,10 @@
+var worker;
+function startWorker() {
+    worker = new Worker("js/worker.js");
+    worker.onmessage = function(event) {
+        document.getElementById("output").innerHTML += '<li>' + event.data + '</li>';
+    };
+}
+function stopWorker() {
+    worker.terminate();
+}
