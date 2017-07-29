20fa01e65cc4ff7b6e425a8b4eed1663a8c123ac
npark8 <napark8@gmail.com>
2017-07-29 01:46:16 -0400

    update README.md

--- module4 - Implementing Program Flow using JavaScript/exercises/exercise4 - Checking Null Values in JavaScript/js/script.js
+++
@@ -1,8 +0,0 @@
-var testVariable = null;
-var isUndefined = (testVariable === undefined);
-var isNull = (testVariable === null);
-var isEmpty = (testVariable === '');
-
-document.getElementById('isUndefined').innerHTML = isUndefined;
-document.getElementById('isNull').innerHTML = isNull;
-document.getElementById('isEmpty').innerHTML = isEmpty;
